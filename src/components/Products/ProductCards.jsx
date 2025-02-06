3fcf688c4157775b907293014f5780e87d7f092b
xxadikk <alisherrov1@gmail.com>
2025-02-06 08:35:43 +0600

    update card

--- src/components/Products/ProductCards.jsx
+++ src/components/Products/ProductCards.jsx
@@ -1,17 +1,32 @@
 import { Card } from "antd";
 import "../Products/ProductCards.css";
 import Meta from "antd/es/card/Meta";
+import { HeartFilled, HeartOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const ProductCards = () => {
+  const [liked, setLiked] = useState(true);
+  const handleLikeClick = async () => {
+    const newLikedState = !liked;
+    setLiked(newLikedState);
+
+    // try {
+    //   await axios.post('/api/update-like', {
+    //     productId,
+    //     liked: newLikedState,
+    //   });
+    // } catch (error) {
+    //   console.error('Ошибка при обновлении состояния лайка:', error);
+    // }
+  };
   return (
     <div className="container">
       <div className="productCards">
         <h1>All products</h1>
         <div className="productCards__body">
           <Card
-            style={{
-              width: 320,
-            }}
+            className="productCard"
             cover={
               <img
                 alt="example"
@@ -24,7 +39,24 @@ const ProductCards = () => {
               />
             }
           >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
+            <motion.div
+              motion
+              className="productCard-like"
+              onClick={handleLikeClick}
+              whileTap={{ scale: 0.8 }}
+              animate={{
+                scale: liked ? 1 : 1,
+                color: liked ? "#ff4d4f" : "#000",
+              }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              {liked ? (
+                <HeartFilled className="productCard-like__icon" />
+              ) : (
+                <HeartOutlined className="productCard-like__icon" />
+              )}
+            </motion.div>
+            <h3>Selected Walnuts</h3>
           </Card>
           <Card
             style={{
